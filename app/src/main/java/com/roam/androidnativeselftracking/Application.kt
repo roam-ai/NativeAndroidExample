e3c1254c161f5ce3bf4e172df320afc3e4f4a273
birju-roam <birju@roam.ai>
2025-03-26 16:47:54 +0530

    added user flow

--- app/src/main/java/com/roam/androidnativeselftracking/Application.kt
+++ app/src/main/java/com/roam/androidnativeselftracking/Application.kt
@@ -7,6 +7,7 @@ class Application: Application() {
     override fun onCreate() {
         super.onCreate()
         //Add Your project publish key
-        Roam.initialize(this, "PUBLISH_KEY")
+        //Roam.initialize(this, "PUBLISH_KEY")
+        Roam.initialize(this, "48e2b50e7a19d667f6788c3063d08e7e24d58bd8fcf80fe0890484761acaff6f")
     }
 }
